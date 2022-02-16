12b8b8dc295e6a8b94dcd1dea5ec3fd3c814af78
faris <faris38kanbur@gmail.com>
2022-02-16 18:54:09 +0100

    add style for avatar

--- src/SupportEngine/Avatar.js
+++ src/SupportEngine/Avatar.js
@@ -5,8 +5,17 @@ const Avatar = (props) => {
   const [hovered, setHovered] = useState(false);
 
   return (
-    <div style={{ position: "fixed", bottom: "24px", right: "24px" }}>
-      <div>Hey it's Faris</div>
+    <div style={props.style}>
+      <div
+        className="transition-3"
+        style={{
+          ...styles.avatarHello,
+          ...{ opacity: hovered ? "1" : "0" },
+        }}
+      >
+        Hey it's Faris
+      </div>
+
       <div
         className="transition-3"
         onMouseEnter={() => setHovered(true)}
--- src/SupportEngine/index.js
+++ src/SupportEngine/index.js
@@ -4,7 +4,7 @@ import Avatar from "./Avatar";
 const SupportEngine = () => {
   return (
     <div>
-      <Avatar />
+      <Avatar style={{ position: "fixed", bottom: "24px", right: "24px" }} />
     </div>
   );
 };
